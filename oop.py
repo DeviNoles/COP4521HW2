abe250e8db1985a813189804082715a736c0f10e
DevinM4 <devin8821@gmail.com>
2021-01-27 16:44:26 -0500

    Fixed bugs

--- oop.py
+++ oop.py
@@ -16,15 +16,27 @@ class Student:
             self.gender = gender
             self.level = level
             self.gpa = gpa
+            
+        if(float(self.gpa)>4):
+           # print("DA GP IS ", self.gpa)
+            self.gpa=4
+
+        
     def show_myself(self):
         print("First Name: "+self.firstName)
         print("Last Name: "+self.lastName)
         print("Gender: "+self.gender)
         print("Class Level: "+self.level)
         print("GPA: "+str(self.gpa))
+
+        
     def study_time(self,st):
-        gp = int(self.gpa) + math.log(st)
+        gp = float(self.gpa) + math.log10(st)
+       # print(math.log10(st))
         self.gpa = gp
+       # print(self.gpa)
+        if(float(self.gpa)>4):
+            self.gpa=4
 s1 = Student("Mike", "Barnes", "Male", "Freshman", 4)
 s2 = Student("Jim", "Nickerson", "Male", "Sophomore", 3)
 s3 = Student("Jack", "Indabox", "Male", "Junior", 2.5)
@@ -34,4 +46,6 @@ s5 = Student("Mary", "Scott", "Female", "Senior", 2.7)
 student_list = [s1,s2,s3,s4,s5]
 
 for i in student_list:
+    i.show_myself()
+    i.study_time(5)
     i.show_myself()
